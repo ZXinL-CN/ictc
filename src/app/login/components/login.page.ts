8ff62959f95758395d9a411b02e043eddc401024
ZXinL-CN <xinliang.zhu@qq.com>
2023-07-08 18:04:55 +0800

    feature(): add user login & background

--- src/app/login/components/login.page.ts
+++ src/app/login/components/login.page.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/share/services/storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage  implements OnInit {
+export class LoginPage implements OnInit {
+  public username: string = '';
+  public password: string = '';
+  constructor(
+    public _router: Router,
+    public _storageService: StorageService,
+  ) { }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {}
+  public async onLogin() {
+    await this._storageService.setItem('userInfo', { username: this.username, password: this.password });
+    this._router.navigate(['/tabs/my']);
+  }
+
+  public goHome() {
+    this._router.navigate(['/tabs/home']);
+  }
+
+  get enLogin() {
+    return this.username && this.password;
+  }
 
 }
--- src/app/my/components/my.page.ts
+++ src/app/my/components/my.page.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { StorageService } from 'src/app/share/services/storage.service';
 
 @Component({
   selector: 'app-my',
   templateUrl: 'my.page.html',
   styleUrls: ['my.page.scss']
 })
-export class MyPage {
-
+export class MyPage implements OnInit {
+  public usserInfo: {
+    username: string;
+  } = {
+      username: ''
+    };
   constructor(
     private translate: TranslateService,
     public router: Router,
-  ) {}
+    public _storageService: StorageService,
+  ) { }
+
+  async ngOnInit() {
+    this.usserInfo = await this._storageService.getItem('userInfo');
+  }
 
   public switchLang() {
     console.log(this.translate);
     this.router.navigate(['./tabs/langset'])
+  }
+
+  public onLogout() {
+    this._storageService.removeItem('userInfo');
+    this.router.navigate(['./login']);
   }
 
 }
